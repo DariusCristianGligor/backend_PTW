5fa13e52c84264eae2ff0dd6b9528a58ff10e660
DariusCristianGligor <87867786+DariusCristianGligor@users.noreply.github.com>
2021-12-07 21:26:31 +0200

    Backend

--- src/ReviewNow/Startup.cs
+++ src/ReviewNow/Startup.cs
@@ -1,5 +1,6 @@
 using Application;
 using Application.Services;
+using Auth0.AspNetCore.Authentication;
 using AutoMapper;
 using Infrastructure;
 using Infrastructure.Repository;
@@ -25,6 +26,22 @@ namespace ReviewNow
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Cookie configuration for HTTP to support cookies with SameSite=None
+
+
+            // Cookie configuration for HTTPS
+            // services.Configure<CookiePolicyOptions>(options =>
+            // {
+            //    options.MinimumSameSitePolicy = SameSiteMode.None
+            // });
+            //---auth0Configuration
+            services
+                .AddAuth0WebAppAuthentication(options => {
+                    options.Domain = Configuration["Auth0:dev-kax74vow.us.auth0.com"];
+                    options.ClientId = Configuration["Auth0:bj1sn1EwwyjtgceQBxTCCuMzsA2UsnMd"];
+                });
+            //------------------------
+            services.AddControllersWithViews();
             var mapperConfig = new MapperConfiguration(mc =>
             {
                 mc.AddProfile(new MappingProfile());
@@ -35,13 +52,14 @@ namespace ReviewNow
 
             services.AddMvc();
 
-            // Register the Swagger generator, defining 1 or more Swagger documents
+            // Register the Swagger generator, defining 1 or more Swagger documents////to use swager
             services.AddSwaggerGen();
-
+            // conexion databasae
             services.AddDbContext<ReviewNowContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
+            //dependency injection
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IWrapperPlaceRepository, WrapperPlaceRepository>();
             services.AddScoped<IWrapperStringPathReviewRepository, WraperStringPAthReviewRepository>();
@@ -66,7 +84,10 @@ namespace ReviewNow
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        {   //---auth0Configuration
+            //app.UseAuthentication();
+            //app.UseAuthorization();
+            //--------------------
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
